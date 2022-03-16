0264238f65ad0998e4ed6f5e9c6206b5266ac806
Mai Thang <Mai Thang@SYAORAN>
2022-03-16 15:36:16 +0700

    Add project files.

---
+++ LabWeb_MaiThang/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LabWeb_MaiThang.MvcApplication" Language="C#" %>
---
+++ LabWeb_MaiThang/Models/Giohang.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LabWeb_MaiThang.Models
+{
+    public class Giohang
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        public int masach { get; set; }
+        public string tensach { get; set; }
+        public string hinh { get; set; }
+        public Double giaban { get; set; }
+        public int iSoluong { get; set; }
+        public Double dThanhtien
+        {
+            get { return iSoluong * giaban; }
+        }
+        public Giohang(int id)
+        {
+            masach = id;
+            Sach sach = data.Saches.Single(n => n.masach == masach);
+            tensach = sach.tensach;
+            hinh = sach.hinh;
+            giaban = double.Parse(sach.giaban.ToString());
+            iSoluong = 1;
+        }
+    }
+}
